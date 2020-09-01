4159d6c1e3a215dc4cb620fd49fea0c61fd634dd
journey-ad <journey-ad@users.noreply.github.com>
2020-09-01 11:54:28 +0000

    deploy: 2b5c8ee105e576951899b8143ea375c26da623e5

--- js/js.js
+++ js/js.js
@@ -177,11 +177,62 @@ $("#update_style").change(function () {
   }
 });
 
-if (/^\/archives\//.test(location.pathname) && pub_date && Date.now() - pub_date >= 90 * 24 * 60 * 60 * 1000) {
+// 文章过时提示
+if (/^\/archives\/.+/.test(location.pathname) && pub_date && Date.now() - pub_date >= 90 * 24 * 60 * 60 * 1000) {
   $('.outdated-notify .num').text(Math.floor((Date.now() - pub_date) / (24 * 60 * 60 * 1000)))
   $('.outdated-notify').show()
-}
+};
 
+// 文章页后退按钮
 $('a.back').click(function () {
   history.go(-1);
-})
+});
+
+// 返回顶部按钮
+(function () {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    return
+  }
+
+  var isShow = false, lock = false;
+  var $btn = $('.back-to-top');
+
+  $(document).scroll(function () {
+    if (lock) return
+
+    if ($(this).scrollTop() >= 1000) {
+      if (!isShow) $btn.addClass('load')
+      isShow = true
+    } else {
+      if (isShow) {
+        $btn.removeClass('load')
+        isShow = false
+      }
+    }
+  })
+
+  $btn.click(function () {
+    lock = true
+    $btn.addClass('ani-leave')
+
+    $("html, body").animate({ scrollTop: 0 }, 800);
+
+    setTimeout(function () {
+      $btn.removeClass('ani-leave').addClass('leaved')
+    }, 390)
+
+    setTimeout(function () {
+      $btn.addClass('ending')
+    }, 120)
+
+    setTimeout(function () {
+      $btn.removeClass('load')
+    }, 1500);
+
+    setTimeout(function () {
+      lock = false
+      isShow = false
+      $btn.removeClass('leaved ending')
+    }, 2000);
+  })
+})();
